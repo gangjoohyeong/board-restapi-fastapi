15dbe0628f1d616750ff1becbd8a739de83ec2e9
kangjoohyeong <bles@kakao.com>
2023-11-20 21:22:57 +0900

    feat: add main

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+import uvicorn
+
+app = FastAPI()
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+@app.get("/")
+def hello() -> dict:
+    return {"message": "Hello World"}
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
